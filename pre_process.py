0699d0d0bd019980893931407ecec7ce3b00ecdd
Yang Liu <foamliu@yeah.net>
2018-09-05 15:34:24 +0800

    update

--- pre_process.py
+++ pre_process.py
@@ -19,23 +19,26 @@ def extract(package):
 
 def gen_gt_file(folder, usage):
     original_images_root = os.path.join(folder, original_images_key)
-    folders = [f for f in os.listdir(original_images_root) if os.path.isdir(os.path.join(original_images_root, f))]
+    original_images_folders = [f for f in os.listdir(original_images_root) if os.path.isdir(os.path.join(original_images_root, f))]
     label_images_root = os.path.join(folder, label_images_key)
 
     gt_list = []
-    for folder in folders:
-        parent = os.path.join(original_images_root, folder)
+    for original_images_folder in original_images_folders:
+        parent = os.path.join(original_images_root, original_images_folder)
         files = [f for f in os.listdir(parent) if f.lower().endswith('.bmp')]
+        label_images_folder = original_images_root.split('.')[0] + '_labelMark'
 
         for file in files:
             original_path = os.path.join(parent, file)
-            label_path = os.path.join(os.path.join(label_images_root, folder), file)
+            label_path = os.path.join(os.path.join(label_images_root, label_images_folder), file)
             if not os.path.isfile(label_path):
                 print('cannot find: {}'.format(label_path))
+                continue
+
             item = {'original_image': original_path, 'label_path': label_path}
             gt_list.append(item)
-            print(item)
 
+    print(len(gt_list))
     with open('{}_gt_file.json'.format(usage), 'w') as file:
         json.dump(gt_list, file, indent=4)
 
