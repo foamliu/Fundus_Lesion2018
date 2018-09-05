485ed9634f69330ffe72f7a9bef820edca159761
Yang Liu <foamliu@yeah.net>
2018-09-05 15:38:42 +0800

    update

--- data_generator.py
+++ data_generator.py
@@ -1,36 +1,39 @@
+import json
+
 import cv2 as cv
 import numpy as np
 from keras.applications.vgg16 import preprocess_input
 from keras.utils import Sequence
 from keras.utils import to_categorical
 
-from config import img_rows, img_cols, batch_size
-from config import num_classes
+from config import img_rows, img_cols, batch_size, num_classes
 
 
 class DataGenSequence(Sequence):
     def __init__(self, usage):
         self.usage = usage
+        if self.usage == 'train':
+            gt_file = 'train_gt_file.json'
+        else:
+            gt_file = 'valid_gt_file.json'
 
-        with open('{}_ids.txt'.format(usage), 'r') as f:
-            ids = f.read().splitlines()
-            self.ids = list(map(int, ids))
+        with open(gt_file, 'r') as file:
+            self.samples = json.load(file)
 
-        with open('names.txt', 'r') as f:
-            self.names = f.read().splitlines()
+        np.random.shuffle(self.samples)
 
     def __len__(self):
-        return int(np.ceil(len(self.ids) / float(batch_size)))
+        return int(np.ceil(len(self.samples) / float(batch_size)))
 
     def __getitem__(self, idx):
         i = idx * batch_size
 
-        length = min(batch_size, (len(self.ids) - i))
+        length = min(batch_size, (len(self.samples) - i))
         X = np.empty((length, img_rows, img_cols, 3), dtype=np.float32)
         Y = np.empty((length, img_rows, img_cols, num_classes), dtype=np.float32)
 
         for i_batch in range(length):
-            id = self.ids[i + i_batch]
+            sample = self.samples[i + i_batch]
             name = self.names[id]
             image = get_image(name)
             category = get_category(id)
@@ -46,7 +49,7 @@ class DataGenSequence(Sequence):
         return X, Y
 
     def on_epoch_end(self):
-        np.random.shuffle(self.ids)
+        np.random.shuffle(self.samples)
 
 
 def train_gen():
