10c6b16bd8a27e17908759efa6f91eabef9eeb54
Yang Liu <foamliu@yeah.net>
2018-09-05 14:59:17 +0800

    update

--- pre_process.py
+++ pre_process.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 import os
 import zipfile
+import json
 
+from config import train_folder, valid_folder, original_images_key, label_images_key
 
 def ensure_folder(folder):
     if not os.path.exists(folder):
@@ -15,9 +17,36 @@ def extract(package):
         zip_ref.extractall('data')
 
 
+def gen_gt_file(folder, usage):
+    root = os.path.join('data', folder)
+    original_images = os.path.join(root, original_images_key)
+    folders = [f for f in os.listdir(original_images) if os.path.isdir(os.path.join(original_images_folder, f))]
+    label_images = os.path.join(root, label_images_key)
+
+    gt_list = []
+    for folder in folders:
+        parent = os.path.join(original_images, folder)
+        files = [f for f in os.listdir(parent) if f.lower().endswith('.bmp')]
+
+        for file in files:
+            original_path = os.path.join(parent, file)
+            label_path = os.path.join(os.path.join(label_images, folder), file)
+            if not os.path.isfile(label_path):
+                print('cannot find: {}'.format(label_path))
+            item = {'original_image': original_path, 'label_path': label_path}
+            gt_list.append(item)
+            print(item)
+
+    with open('{}_gt_file.json'.format(usage), 'w') as file:
+        json.dump(gt_list, file, indent=4)
+
+
 if __name__ == '__main__':
     ensure_folder('data')
 
     extract('ai_challenger_fl2018_trainingset')
     extract('ai_challenger_fl2018_validationset')
     extract('ai_challenger_fl2018_testset')
+
+    gen_gt_file(train_folder, 'train')
+    gen_gt_file(valid_folder, 'valid')
