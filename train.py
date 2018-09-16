66f2a2b7fb9f14fa70672e806b54db633859266a
Yang Liu <foamliu@yeah.net>
2018-09-16 10:27:57 +0800

    update

--- train.py
+++ train.py
@@ -16,10 +16,10 @@ if __name__ == '__main__':
 
     # Callbacks
     tensor_board = keras.callbacks.TensorBoard(log_dir='./logs', histogram_freq=0, write_graph=True, write_images=True)
-    model_names = checkpoint_models_path + 'model.{epoch:02d}-{val_loss:.4f}.hdf5'
-    model_checkpoint = ModelCheckpoint(model_names, monitor='val_loss', verbose=1, save_best_only=True)
-    early_stop = EarlyStopping('val_loss', patience=patience)
-    reduce_lr = ReduceLROnPlateau('val_loss', factor=0.5, patience=int(patience / 4), verbose=1)
+    model_names = checkpoint_models_path + 'model.{epoch:02d}-{val_acc:.4f}.hdf5'
+    model_checkpoint = ModelCheckpoint(model_names, monitor='val_acc', verbose=1, save_best_only=True)
+    early_stop = EarlyStopping('val_acc', patience=patience)
+    reduce_lr = ReduceLROnPlateau('val_acc', factor=0.5, patience=int(patience / 4), verbose=1)
 
 
     class MyCbk(keras.callbacks.Callback):
@@ -31,7 +31,7 @@ if __name__ == '__main__':
             fmt = checkpoint_models_path + 'model.%02d-%.4f.hdf5'
             highest_acc = get_highest_acc()
             if float(logs['val_acc']) > highest_acc:
-                self.model_to_save.save(fmt % (epoch, logs['val_loss']))
+                self.model_to_save.save(fmt % (epoch, logs['val_acc']))
 
 
     # Load our model, added support for Multi-GPUs
